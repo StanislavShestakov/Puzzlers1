d02438e2ef0106ff50d130e98eb7fb332481fa89
Stas <w3oks@mail.ru>
2019-05-30 14:51:31 +0400

    bubble any not primitive types sort

--- src/main/java/BestExmaples/BubbleSortAnyArray.java
+++ src/main/java/BestExmaples/BubbleSortAnyArray.java
@@ -8,27 +8,29 @@ import java.util.Arrays;
 public class BubbleSortAnyArray {
     public static void main(String[] args) {
 
-        Integer arr[] ={3,60,35,2,45,320,5};
-        System.out.println(Arrays.toString(arr));
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
+        Integer integers[] ={3,60,35,2,45,320,5};
+        System.out.println(Arrays.toString(integers));
+        System.out.println(Arrays.toString(bubbleSort(integers)));
+        System.out.println("\n==========================\n");
+        String strings[] = {"apple","carrot","tomato","peas","barries","potato","cerries"};
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(bubbleSort(strings)));
     }
 
     static <E extends Comparable<E>> E[] bubbleSort(E[] arr) {
-//        int n = arr.length;
-//        int temp = 0;
-//        for(int i=0; i < n; i++) {
-//            for (int j = 1; j < (n - i); j++) {
-//                if (arr[j - 1] > arr[j]) {
-//                    //swap elements
-//                    temp = arr[j - 1];
-//                    arr[j - 1] = arr[j];
-//                    arr[j] = temp;
-//                }
-//
-//            }
-//        }
+        int n = arr.length;
+        E temp;
+        for(int i=0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1].compareTo(arr[j]) > 0) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
         return arr;
     }
 
-    }
+}
