de40a5f1b51868f4635e4c96a898a3a4186fd8b7
Stas <w3oks@mail.ru>
2019-05-30 16:58:51 +0400

    bubble any not primitive types sort without util classes

---
+++ src/main/java/BestExmaples/bubbleSort/Loader.java
@@ -0,0 +1,28 @@
+package BestExmaples.bubbleSort;
+
+import java.util.Arrays;
+
+/**
+ * Created by Stas on 29.05.2019.
+ */
+public class Loader {
+    public static void main(String[] args) {
+
+        System.out.println("\n Sort Integers ==========================\n");
+        Integer integers[] ={3,60,35,2,45,320,5};
+        System.out.println(Arrays.toString(integers));
+        System.out.println(Arrays.toString(SuperBubbleSort.sort(integers)));
+        System.out.println("\n Sort Strings ==========================\n");
+        String strings[] = {"apple","carrot","tomato","peas","barries","potato","cerries"};
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(SuperBubbleSort.sort(strings)));
+        System.out.println("\n Sort ints ==========================\n");
+        int []  intPrimitives = {3,60,35,2,45,320,5};
+        System.out.println(Arrays.toString(intPrimitives));
+        System.out.println(Arrays.toString(SuperBubbleSort.sort(intPrimitives)));
+
+    }
+
+
+
+}
---
+++ src/main/java/BestExmaples/bubbleSort/SuperBubbleSort.java
@@ -0,0 +1,89 @@
+package BestExmaples.bubbleSort;
+
+import java.util.Arrays;
+
+/**
+ * Created by Stas on 30.05.2019.
+ */
+public class SuperBubbleSort {
+    static <E extends Comparable<E>> E[] sort(E[] arr) {
+        int n = arr.length;
+        E temp;
+        for(int i=0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1].compareTo(arr[j]) > 0) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+        return arr;
+    }
+    static <E extends Comparable<E>> E[] sort(Object object) {
+        E[] temp = null;
+        if (object instanceof int[]) {
+            int[] arr = (int[]) object;
+            Integer[] result = new Integer[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Integer.valueOf(arr[i]);
+            }
+            temp = (E[]) sort(result);
+        } else if (object instanceof double[]) {
+            double[] arr = (double[]) object;
+            Double[] result = new Double[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Double.valueOf(arr[i]);
+            }
+            temp = (E[])  sort(result);
+
+        } else if (object instanceof short[]) {
+            short[] arr = (short[]) object;
+            Short[] result = new Short[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Short.valueOf(arr[i]);
+            }
+            temp = (E[])  sort(result);
+        } else if (object instanceof char[]) {
+            char[] arr = (char[]) object;
+            Character[] result = new Character[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Character.valueOf(arr[i]);
+            }
+            temp = (E[])   sort(result);
+        } else if (object instanceof byte[]) {
+            Byte[] arr = (Byte[]) object;
+            Byte[] result = new Byte[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Byte.valueOf(arr[i]);
+            }
+            temp = (E[])   sort(result);
+        } else if (object instanceof long[]) {
+            Long[] arr = (Long[]) object;
+            Long[] result = new Long[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Long.valueOf(arr[i]);
+            }
+            temp = (E[])   sort(result);
+        } else if (object instanceof float[]) {
+            Float[] arr = (Float[]) object;
+            Float[] result = new Float[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Float.valueOf(arr[i]);
+            }
+            temp = (E[])     sort(result);
+        } else if (object instanceof boolean[]) {
+            Boolean[] arr = (Boolean[]) object;
+            Boolean[] result = new Boolean[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                result[i] = Boolean.valueOf(arr[i]);
+
+            }
+            temp = (E[]) sort(result);
+        }else{
+                throw new RuntimeException("not supported type of array!");
+            }
+        return temp;
+    }
+}
