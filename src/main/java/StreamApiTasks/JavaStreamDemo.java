17c07a256f88b3cb7ee5ae9f69116cd6d450f891
Dmitriy <dgsubscr23@gmail.com>
2019-06-05 04:46:18 +0300

    task 4 complete

--- src/main/java/StreamApiTasks/JavaStreamDemo.java
+++ src/main/java/StreamApiTasks/JavaStreamDemo.java
@@ -1,14 +1,15 @@
 package StreamApiTasks;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class JavaStreamDemo {
-    private static final List<String> DATA_1 = Arrays.asList("a1", "a2", "a3", "a4");
+    private static final List<String> DATA_1 = Arrays.asList("a1", "a2", "a4", "a3", "a1", "a4");
     private static final List<People> DATA_2 = Arrays.asList( new People("Вася", 16, Sex.MAN), new People("Петя", 23, Sex.MAN), new People("Елена", 42, Sex.WOMEN), new People("Иван Иванович", 69, Sex.MAN));
 
     public static void main(String[] args) {
-        /*System.out.println(task1_1());
+       /* System.out.println(task1_1());
         System.out.println(task1_2());
         System.out.println(task1_3());
         System.out.println(task1_4());
@@ -17,9 +18,19 @@ public class JavaStreamDemo {
 
         System.out.println(Arrays.toString(task2_1()));
         System.out.println(task2_2());
-        System.out.println(task2_3());*/
+        System.out.println(task2_3());
 
         System.out.println(task3_1());
+        System.out.println(task3_2());
+        System.out.println(task3_3());
+        System.out.println(task3_4());*/
+
+        System.out.println(Arrays.toString(task4_1()));
+        System.out.println(Arrays.toString(task4_2()));
+        System.out.println(Arrays.toString(task4_3()));
+        System.out.println(Arrays.toString(task4_4()));
+        System.out.println(Arrays.toString(task4_5()));
+        System.out.println(Arrays.toString(task4_6()));
     }
 
     private static long task1_1(){
@@ -59,8 +70,46 @@ public class JavaStreamDemo {
     }
 
     private static boolean task3_1(){
-        return DATA_1.stream().allMatch(e -> e.equals("a1"));
+        return DATA_1.stream().anyMatch(e -> e.equals("a1"));
     }
+
+    private static boolean task3_2(){
+        return DATA_1.stream().anyMatch(e -> e.equals("a8"));
+    }
+
+    private static boolean task3_3(){
+        return DATA_1.stream().allMatch(e -> e.contains("1"));
+    }
+
+    private static boolean task3_4(){
+        return DATA_1.stream().noneMatch(e -> e.equals("a7"));
+    }
+
+    private static String[] task4_1(){
+        return DATA_1.stream().sorted().toArray(String[]::new);
+    }
+
+    private static String[] task4_2(){
+        return DATA_1.stream().sorted(Comparator.reverseOrder()).toArray(String[]::new);
+    }
+
+    private static String[] task4_3(){
+        return DATA_1.stream().distinct().sorted().toArray(String[]::new);
+    }
+
+    private static String[] task4_4(){
+        return DATA_1.stream().distinct().sorted(Comparator.reverseOrder()).toArray(String[]::new);
+    }
+
+    private static People[] task4_5(){
+        return DATA_2.stream().sorted(Comparator.comparing(a -> a.name)).toArray(People[]::new);
+    }
+
+    private static People[] task4_6(){
+        return DATA_2.stream().sorted(Comparator.comparing((People p) -> p.sex).thenComparing(p -> p.name)).toArray(People[]::new);
+    }
+
+
 
 
 
