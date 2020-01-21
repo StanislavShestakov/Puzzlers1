335a64fd7be5cc1669e4007e8222570128a75208
Stanislav <w3orks@mail.ru>
2020-01-21 15:08:29 +0300

    hex

---
+++ src/main/java/expressivePuzzlers/Elementary.java
@@ -0,0 +1,8 @@
+package expressivePuzzlers;
+
+public class Elementary {
+    public static void main(String[] args) {
+        System.out.println(12345 + 5432l);
+        System.out.println(12345 + 5432L);
+    }
+}
