2bfd2df61704a1164f3505de323113a5bfae88d8
qweky <w3orks@mail.ru>
2020-09-17 15:59:05 +0300

    RemoveDuplicates wrong solution but works

---
+++ src/main/java/BestExmaples/leetcode/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+package BestExmaples.leetcode;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(arr));
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int validSize = 0;
+        int val = -2;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != val){
+                nums[validSize] = nums[i];
+                validSize++;
+                val = nums[i];
+            }
+        }
+        return validSize;
+    }
+}
