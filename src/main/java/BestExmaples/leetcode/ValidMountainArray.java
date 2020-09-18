5bc9133f78f34179aea50987db75fa39a6166b97
qweky <w3orks@mail.ru>
2020-09-18 13:19:07 +0300

    ValidMountainArray brokenSolution

---
+++ src/main/java/BestExmaples/leetcode/ValidMountainArray.java
@@ -0,0 +1,67 @@
+package BestExmaples.leetcode;
+
+import java.util.Arrays;
+
+public class ValidMountainArray {
+    //https://leetcode.com/explore/featured/card/fun-with-arrays/527/searching-for-items-in-an-array/3251/
+    public static void main(String[] args) {
+        int[] arr = {3,7,6,4,3,0,1,0};
+        System.out.println(validMountainArray(arr));
+        System.out.println(Arrays.toString(arr));
+        int[] arr1 = {2,1};
+        System.out.println(validMountainArray(arr1));
+        System.out.println(Arrays.toString(arr1));
+        int[] arr2 = {0,3,2,1};
+        System.out.println(validMountainArray(arr2));
+        System.out.println(Arrays.toString(arr2));
+        int[] arr3 = {1,3,2};
+        System.out.println(validMountainArray(arr3));
+        System.out.println(Arrays.toString(arr3));
+        int[] arr4 = {2,1,2,3,5,7,9,10,12,14,15,16,18,14,13};
+        System.out.println(validMountainArray(arr4));
+        System.out.println(Arrays.toString(arr4));
+    }
+    public static boolean validMountainArray(int[] A) {
+        if(A.length < 3){
+            return false;
+        }
+        int veerAmount = 0;
+        boolean isAscending = false;
+        boolean isDescending = false;
+        boolean isFirstAscending = false;
+        boolean isFirstDescending = false;
+        for (int i = 1; i < A.length; i++) {
+            if(A[i] == A[i - 1]){
+                return false;
+            }
+            if(A[i] > A[i-1]){
+                System.out.println("Ascending");
+                if(!isAscending){
+                    veerAmount++;
+                    isAscending = true;
+                    isDescending = false;
+                    if(!isFirstAscending && !isFirstDescending){
+                        isFirstAscending = true;
+                    }
+                }
+
+            }
+            if(A[i] < A[i - 1] && veerAmount > 0){
+                System.out.println("Descending");
+                if(!isDescending) {
+                    veerAmount++;
+                    isDescending = true;
+                    isAscending = false;
+                    isFirstDescending = true;
+                }
+            }
+
+        }
+       if(veerAmount  == 2 && isFirstAscending){
+            return true;
+        }else{
+            return false;
+       }
+
+    }
+}
