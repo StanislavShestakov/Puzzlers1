633321265fe07fbaa6307fb340432d743396d464
Admin <w3orks@mail.ru>
2020-09-29 09:19:07 +0300

    Find All Numbers Disappeared in an Array (wrong answer)

--- src/main/java/BestExmaples/leetcode/FindAllNumbersDisappearedInAnArray.java
+++ src/main/java/BestExmaples/leetcode/FindAllNumbersDisappearedInAnArray.java
@@ -7,21 +7,30 @@ import java.util.List;
 public class FindAllNumbersDisappearedInAnArray {
     //https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3270/
     public static void main(String[] args) {
-        int[] arr = {4,3,2,7,8,2,3,1,26};
+        int[] arr = {1,1};
         System.out.println(findDisappearedNumbers(arr));
 
     }
     public static List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> integers = new ArrayList<>();
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        int dblAmount = 0;
         for (int i = 1; i < nums.length; i++) {
+          if(nums[i]  ==  nums[i-1]){
+              dblAmount++;
+          }
           if ((nums[i] - nums[i-1]) > 1){
               int counter = (nums[i] - nums[i-1]);
               for (int j = 1; j < counter; j++) {
-                  integers.add(nums[i-1]+j);
+                  if(dblAmount > 0) {
+                      integers.add(nums[i - 1] + j);
+                      dblAmount--;
+                  }
               }
           }
+        }
+        for (int i = 0; i < dblAmount; i++) {
+            integers.add(nums[nums.length - 1] + i+1);
         }
         return integers;
     }
